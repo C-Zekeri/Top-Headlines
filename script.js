1858b9e9b1b8beac436f809144c9e341be5df063
Chenemi Zekeri <chenemi2@gmail.com>
2020-08-02 16:39:45 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const searchButton = document.getElementById('search-button');
+const input = document.getElementById('search-query');
+const outputSection = document.querySelector('.output');
+
+searchButton.addEventListener('click', searchTopic);
+
+function searchTopic() {
+    let searchQuery = input.value;
+
+    fetch(`https://newsapi.org/v2/top-headlines?q=${searchQuery}&apiKey=c322fecc82854c4a93cbeb3b16b60fd9`)
+        .then(response => response.json())
+        .then(data => console.log(articles));
+}
